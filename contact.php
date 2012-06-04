201622c67de2879d5e8294f8c35dffa312e2ae08
Rob Cowie <rob_cowie@mac.com>
2012-06-04 18:40:25 +0100

    Original content (without unused images)

---
+++ contact.php
@@ -0,0 +1,157 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<?
+	$name = $_POST["Name"];
+	$email = $_POST["Email"];
+	$message = $_POST["Message"];
+						
+	$recipient = "Online Web Contact <mail@christmascrew.co.uk>";
+	
+	$subject = "Online Contact Form from $name";
+
+	$mail_body = "Name:\t".$name."\nEmail:\t".$email."\nMessage:\t".$message;
+    
+	// echo $message;
+	$extra = "From: $email\r\nReply-To: $email\r\n";
+	mail ($recipient, $subject, $mail_body, $extra);
+?>
+
+<html>
+<head>
+
+<meta http-equiv="content-type" content="text/html; charset=utf-8">
+	<title>Christmas Crew ltd</title>
+    <link href="style.css" rel="stylesheet" type="text/css">
+    <style type="text/css">
+    .style1 {
+    	color: #FF0000;
+    	font-weight: bold;
+    }
+    .style2 {color: #FF0000}
+    </style>
+</head>
+
+<body>
+<img alt="" src="images/spacer.gif" width="1" height="20"><br>
+<div class="wrap">
+<div id="top">
+<!--_____________________________________________________top_left_content_____________________________________________________-->
+<div id="top_left_4">
+	<div style="margin-left:5px; margin-top:1px; margin-right:5px; margin-bottom: 10px; background-color:#FFFFFF">
+		<img alt="" src="images/spacer.gif" width="1" height="33"><a href="index.html"><img src="images/logo.gif" width="467" height="98" border="0" alt="Grotto Management Services"></a>
+	</div>
+	<img alt="" src="images/spacer.gif" width="1" height="55"><br>
+	<img alt="" src="images/spacer.gif" width="27" height="8"><br>
+	<img alt="" src="images/spacer.gif" width="1" height="18"><br>
+	<img alt="" src="images/spacer.gif" width="27" height="1"><br>
+	<img alt="" src="images/spacer.gif" width="1" height="17"><br>
+	<img alt="" src="images/spacer.gif" width="27" height="1"><br>
+	<img alt="" src="images/spacer.gif" width="1" height="19"><br>
+	<img alt="" src="images/spacer.gif" width="27" height="1"><br>
+	<img alt="" src="images/spacer.gif" width="1" height="18"><br>
+	<img alt="" src="images/spacer.gif" width="27" height="1"><br>
+	<img alt="" src="images/spacer.gif" width="1" height="18"><br>
+
+</div>
+<!--_____________________________________________________end_top_left_content_____________________________________________________-->
+
+<!--_____________________________________________________top_right_content_____________________________________________________-->
+<div id="top_right">
+<div align="right">
+  <div style="margin-top:5px"><img src="images/m05.gif" width="80" height="75" alt="bell"></div>
+  <br>
+    <img alt="" src="images/spacer.gif" width="1" height="4"><br>
+    <br>
+</div>
+<div style="margin-left:24px; margin-right:24px " class="white">
+<img alt="" src="images/txt1.jpg"><br>
+<span class="bigger"><strong><img alt="" src="images/spacer.gif" width="1" height="10">Contact us </strong></span><br>
+<img alt="" src="images/line.jpg"><br>
+<img alt="" src="images/spacer.gif" width="1" height="14"><br>
+“.. If you would like to know more, please contact us via the link below or give us a call..”<br>
+<img alt="" src="images/spacer.gif" width="1" height="14"><br>
+<img alt="" src="images/line.jpg"><br>
+<img alt="" src="images/spacer.gif" width="1" height="5"><br>
+<div align="right" style="margin-right:8px ">Alastair Cowie<br>
+<a href="#" class="brown a">Grotto Management Services</a></div>
+</div>
+</div>
+<!--_____________________________________________________________end_top_right_content_____________________________________________________-->
+</div>
+
+<div id="content" class="clear">
+<!--_____________________________________________________left_block_____________________________________________________-->
+<div id="left_block">
+<div class="inner_left_block t sm">
+&bull;&nbsp;&nbsp;&nbsp;<a href="index.html" class="style1">Home</a><br>
+<div style="margin-top:8px; margin-bottom:8px; height:1px; background:#E9E9E9 "><img alt="" src="images/spacer.gif"></div>
+&bull;&nbsp;&nbsp;&nbsp;<a href="services.html" class="style1">Our Services </a><br>
+<div style="margin-top:8px; margin-bottom:8px; height:1px; background:#E9E9E9 "><img alt="" src="images/spacer.gif"></div>
+&bull;&nbsp;&nbsp;&nbsp;<a href="gallery.html" class="style1">Gallery</a><br>
+<div style="margin-top:8px; margin-bottom:8px; height:1px; background:#E9E9E9 "><img alt="" src="images/spacer.gif"></div>
+&bull;&nbsp;&nbsp;&nbsp;<a href="contact.html" class="style1">Contact Us </a><br>
+<br><p align="left" style="margin-top:15px"><br>
+</p>
+</div>
+	 <div id="sponsor">
+		<a href="http://www.sheffieldsteelers.co.uk" target="_blank">
+		<img src="images/steelerslogocolour_small.jpg" alt="Sheffield Steelers" width="140" height="138" border="0"></a><br>
+		Christmas Crew Ltd. Proud sponsors to the Sheffield Steelers Ice Hockey Club	 </div>
+</div>
+
+<!--_____________________________________________________end_left_block_____________________________________________________-->
+
+<!--_____________________________________________________right_block_____________________________________________________-->
+<div id="right_block">
+<!--_____________________________________________________left_content_____________________________________________________-->
+<div id="left_content1">
+<div class="inner_left1">
+  <p><strong class="red">Successfully!</strong>
+    <img alt="" src="images/spacer.gif" width="1" height="7">    </p>
+  <p>Your contact request has been submitted successfully. We will contact you shortly regarding to your contact. </p>
+  <p align="center"><strong>Thank you for your attentions.</strong></p>
+</div>
+</div>
+<!--_____________________________________________________________end_left_content_____________________________________________________-->
+
+<!--_____________________________________________________right_content_____________________________________________________-->
+<div id="right_content1">
+<div class="inner_right1">
+<p>
+	<strong class="red">Contact Us</strong>
+	<br><br>
+	Alastair Cowie &amp; Judith Cowie<br><br>
+	<strong>Grotto Management Services</strong>
+	<br>Church Barn,
+	<br>St. Mary's Gate
+	<br>Tickhill,
+	<br>South Yorkshire
+	<br>
+	DN11 9LY
+	<br><br>Tel: 07796 688474
+	<br><br>Email:
+	<a class="style2" href="mailto:mail@christmascrew.co.uk">mail@christmascrew.co.uk</a>
+	<br>
+	<img height="22" width="1" src="images/spacer.gif" alt="">
+	<br>
+	<img alt="" src="images/spacer.gif" width="1" height="20"><br>
+	<a href="http://www.doncaster-chamber.co.uk/" target="_blank"><img src="images/chamber_logo_small.gif" alt="Chamber of Doncaster Chamber" width="150" height="66" border="0" style="margin-left:25px"></a></p>
+</div>
+</div>
+<!--_____________________________________________________________end_right_content_____________________________________________________-->
+</div>
+<!--_____________________________________________________end_right_block_____________________________________________________-->
+
+
+</div>
+	<div id="footer" class="clear bot_txt">
+		<div class="line"><img alt="" src="images/spacer.gif"></div>
+		<br>
+		<div class="copyright">
+			<strong>Christmas Crew Ltd. &copy; 2008</strong>  <span style="margin-left:380px"><a href="contact.html" class="style2">Contact Us</a></span><br>
+			Registered office: 9 St. George's Yard, Castle Street, Farnham, Surrey, GU9 7LW. <br>
+			Registered in England & Wales Company number: 05865603
+		</div>
+	</div>
+</div>
+</body>
+</html>
